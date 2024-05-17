b7afcee90137d6d907381d63dea8cae0a2daf4ac
milenasribeiros <milenassribeiro@gmail.com>
2024-05-17 15:30:04 -0300

    feat: melhorias na qualidade dos codigos

--- cypress/e2e/test/order_products.cy.js
+++ cypress/e2e/test/order_products.cy.js
@@ -4,12 +4,18 @@ describe('Ordenar os produtos', () => {
         cy.get('[data-test="username"]').type("standard_user")
         cy.get('[data-test="password"]').type("secret_sauce")
         cy.get('#login-button').click()
-      });
-    
-    it('Ordenar de A a Z', () => {
-        cy.get('.product_sort_container').select('Name (A to Z)')
-        cy.get('#item_4_title_link > .inventory_item_name').first().should('contain', 'Sauce Labs Backpack')
-        cy.get('#item_3_title_link > .inventory_item_name').should('contain', 'Test.allTheThings() T-Shirt (Red)')
+    });
+
+    it('Opções de ordenação dos produtos', () => {
+
+        it('Ordenar os produtos', () => {
+            cy.fixture('order_produts.json').then((produtos) => {
+                cy.get('.product_sort_container').select(produtos.opcao);
+                cy.get('.inventory_item_name').first().should('have.text', produtos.primeiro);
+                cy.get('.inventory_item_name').eq(1).should('have.text', produtos.segundo);
+            });
+        });
+
     });
     it('Ordenar de Z a A', () => {
         cy.get('.product_sort_container').select('Name (Z to A)')
@@ -27,5 +33,5 @@ describe('Ordenar os produtos', () => {
         cy.get(':nth-child(1) > .pricebar > .inventory_item_price').first().should('contain', '$49.99')
         cy.get(':nth-child(6) > .pricebar > .inventory_item_price').should('contain', '$7.99')
     });
-    
-  })
+
+})
