f69a2c8d47bb7b6932ac4da35a8a4d7b96a13008
milenasribeiros <milenassribeiro@gmail.com>
2024-05-17 16:12:54 -0300

    feat: alterações no teste de ordenação de produtos

--- cypress/e2e/test/order_products.cy.js
+++ cypress/e2e/test/order_products.cy.js
@@ -6,32 +6,23 @@ describe('Ordenar os produtos', () => {
         cy.get('#login-button').click()
     });
 
-    it('Opções de ordenação dos produtos', () => {
-
-        it('Ordenar os produtos', () => {
-            cy.fixture('order_produts.json').then((produtos) => {
-                cy.get('.product_sort_container').select(produtos.opcao);
-                cy.get('.inventory_item_name').first().should('have.text', produtos.primeiro);
-                cy.get('.inventory_item_name').eq(1).should('have.text', produtos.segundo);
+    it('Ordenar os produtos', () => {
+        cy.fixture('order_produts.json').then((data) => {
+          const ordenacaoTest = data.produtos;
+    
+          ordenacaoTest.forEach((produto) => {
+            cy.get('.product_sort_container').select(produto.opcao);
+    
+            if (produto.primeiro && produto.segundo) {
+              cy.get('.inventory_item_name').first().should('have.text', produto.primeiro);
+              cy.get('.inventory_item_name').eq(1).should('have.text', produto.segundo);
+            } else if (produto.firstPrice && produto.lastPrice) {
+              cy.get('.inventory_item').first().find('.inventory_item_price').should('contain', produto.firstPrice);
+              cy.get('.inventory_item').last().find('.inventory_item_price').should('contain', produto.lastPrice);
+            }
+            
             });
         });
 
     });
-    it('Ordenar de Z a A', () => {
-        cy.get('.product_sort_container').select('Name (Z to A)')
-        cy.get('#item_3_title_link > .inventory_item_name').first().should('contain', 'Test.allTheThings() T-Shirt (Red)')
-        cy.get('#item_4_title_link > .inventory_item_name').should('contain', 'Sauce Labs Backpack')
-
-    });
-    it('Ordenar por Price Low to High', () => {
-        cy.get('.product_sort_container').select('Price (low to high)')
-        cy.get(':nth-child(1) > .pricebar > .inventory_item_price').first().should('contain', '$7.99')
-        cy.get(':nth-child(6) > .pricebar > .inventory_item_price').should('contain', '$49.99')
-    });
-    it('Ordenar por Price High to Low', () => {
-        cy.get('.product_sort_container').select('Price (high to low)')
-        cy.get(':nth-child(1) > .pricebar > .inventory_item_price').first().should('contain', '$49.99')
-        cy.get(':nth-child(6) > .pricebar > .inventory_item_price').should('contain', '$7.99')
-    });
-
-})
+});
